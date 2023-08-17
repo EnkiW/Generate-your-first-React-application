ad09d54ea3f8def2f9b7bb901281664d0df4f9f0
EnkiW <ytcahekyttpocto@gmail.com>
2023-08-17 20:45:29 +0300

    App

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <nav>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </nav>
+        </aside>
+    );
+}
+
+export default Sidebar;
