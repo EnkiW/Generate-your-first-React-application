62d1a3e2602a9d715ed42594aa1716d85c2d0059
EnkiW <ytcahekyttpocto@gmail.com>
2023-08-17 21:00:28 +0300

    Fin

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div className="app">
-        <header className="header">Header</header>
-        <div className="main-container">
-          <nav className="sidebar">Sidebar</nav>
-          <main className="content">Content</main>
+    return (
+        <div className="app">
+            <header className="header">Header</header>
+            <div className="all-cont">
+                <nav className="sidebar">Sidebar</nav>
+                <main className="content">Content</main>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
